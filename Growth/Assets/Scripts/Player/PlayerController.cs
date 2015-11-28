a68867380d36b8ac8df29d8c641dfc02ee0adaa5
Jaime (Amy) DiGiovanni <jmdigiov@usc.edu>
2015-11-28 00:31:12 -0800

    Working on basic framework. Can click enemies and get a reaction from the Player Controller.

---
+++ Growth/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //=======Singleton implementation=======//
+    private static PlayerController _instance;
+    public static PlayerController instance
+    {
+        get { return _instance ?? (_instance = new GameObject("PlayerController").AddComponent<PlayerController>()); }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            if (this != _instance)
+                Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    //======================================//
+
+    public void ReceiveMouseInput(GameObject clicked)
+    {
+        if (clicked.tag.Equals("Enemy"))
+        {
+            Debug.Log(":D");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
