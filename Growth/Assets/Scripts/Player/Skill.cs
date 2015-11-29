e31271443cf1e560777a9cccbe17f194d04c3508
Jaime (Amy) DiGiovanni <jmdigiov@usc.edu>
2015-11-28 22:34:03 -0800

    Updated enemy images and started adding skill framework

--- Growth/Assets/Scripts/Player/Skill.cs
+++ Growth/Assets/Scripts/Player/Skill.cs
@@ -5,11 +5,16 @@ public class Skill : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        PlayerController.instance.AddImageToHealthManager(GetComponent<SpriteRenderer>());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnDestroy()
+    {
+        PlayerController.instance.RemoveImageFromHealthManager(GetComponent<SpriteRenderer>());
+    }
 }
